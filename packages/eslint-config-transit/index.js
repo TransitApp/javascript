b783c3872c17ba5c611ad56fce9a703f5b937488
Thomas Lefebvre <lefebvre515@gmail.com>
2017-02-20 16:18:48 -0500

    Forcing the sourceType to be script instead of ES6 modules.

--- packages/eslint-config-transit/index.js
+++ packages/eslint-config-transit/index.js
@@ -1,8 +1,11 @@
+'use strict';
+
 module.exports = {
   extends: 'airbnb-base',
   parserOptions: {
     ecmaVersion: 6,
     ecmaFeatures: {},
+    sourceType: 'script',
   },
   rules: {
     'max-len': ['error', 105, 2],
